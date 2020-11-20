7b51cb4f6349babc11469352d70d41c86b846da7
Lindsey Handley <lindseyhandley@gmail.com>
2020-11-20 23:07:26 +0000

    thunking to aid with parenting

--- main.rkt
+++ main.rkt
@@ -4,38 +4,51 @@
 
 (require-mod rocks)
 (require-mod fire-particles)
+(require-mod hierarchy)
+(require-mod spawners)
 
 (define-classic-rune (trigger-zone exp)
   #:background "blue"
   #:foreground (circle 40 'solid 'blue)
+ ; (thunk
   (unreal-js @~a{
- var actor = new StaticMeshActor(GWorld, {X:@(current-x), Y:@(current-z), Z:@(current-y)});
- var smc = actor.StaticMeshComponent;
- smc.SetMobile();
- smc.SetStaticMesh(StaticMesh.Load('/Engine/BasicShapes/Cube.Cube'));
- actor.StaticMeshComponent.SetVisibility(false);
- actor.StaticMeshComponent.SetCollisionProfileName("trigger");
- actor.StaticMeshComponent.bGenerateOverlapEvents = true;
+ (function(){
+  var actor = new StaticMeshActor(GWorld, {X:@(current-x), Y:@(current-z), Z:@(current-y)});
+  var smc = actor.StaticMeshComponent;
+  smc.SetMobile();
+  smc.SetStaticMesh(StaticMesh.Load('/Engine/BasicShapes/Cube.Cube'));
+  actor.StaticMeshComponent.SetVisibility(false);
+  actor.StaticMeshComponent.SetCollisionProfileName("trigger");
+  actor.StaticMeshComponent.bGenerateOverlapEvents = true;
 
- var overlaps_disabled = false;
+  var overlaps_disabled = false;
  
- actor.OnActorBeginOverlap.Add((overlapped, other) => {
+  actor.OnActorBeginOverlap.Add((overlapped, other) => {
   
-  if(overlaps_disabled){
-   return;
-  }
+   if(overlaps_disabled){
+    return;
+   }
   
-  overlaps_disabled = true;
-  
-  var spawned = @(unreal-js-fragment-content
-                  (if (procedure? exp)
-                      (exp)
-                      exp
-                      ));
+   overlaps_disabled = true;
+    
+   var spawned =
+   @(at
+     [
+        @~a{actor.GetActorLocation().X} ;Ugh, using ~a is gross...
+        @~a{actor.GetActorLocation().Z} ;Flipping Z and Y
+        @~a{actor.GetActorLocation().Y}
+     ]
+     (unreal-js-fragment-content (if (procedure? exp)
+                                     (exp)
+                                     exp 
+                                     )));
                         
-  overlaps_disabled = false;
-  })
- })
+   overlaps_disabled = false;
+   })
+    return actor;
+  })()
+
+ });)
   )
 
 (define-classic-rune-lang my-mod-lang #:eval-from main.rkt
@@ -51,6 +64,8 @@
              #:lang (append-rune-langs #:name main.rkt
                      (my-mod-lang #:with-paren-runes? #t)
                      (rocks:my-mod-lang)
+                     (hierarchy:my-mod-lang)
+                     (spawners:my-mod-lang)
                      (fire-particles:my-mod-lang)))))
 
 
