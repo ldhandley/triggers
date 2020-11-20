18157d7c0b20d4e441a753483cfbc3a429e4b72f
Lindsey Handley <lindseyhandley@gmail.com>
2020-11-20 20:49:36 +0000

    prototype trigger

--- main.rkt
+++ main.rkt
@@ -1,22 +1,56 @@
-#lang codespells
+#lang at-exp codespells
 
 (require triggers/mod-info)
 
-(define-classic-rune (hello)
+(require-mod rocks)
+(require-mod fire-particles)
+
+(define-classic-rune (trigger-zone exp)
   #:background "blue"
   #:foreground (circle 40 'solid 'blue)
-  (spawn-mod-blueprint pak-folder mod-name "HelloWorld"))
+  (unreal-js @~a{
+ var actor = new StaticMeshActor(GWorld, {X:@(current-x), Y:@(current-z), Z:@(current-y)});
+ var smc = actor.StaticMeshComponent;
+ smc.SetMobile();
+ smc.SetStaticMesh(StaticMesh.Load('/Engine/BasicShapes/Cube.Cube'));
+ actor.StaticMeshComponent.SetVisibility(false);
+ actor.StaticMeshComponent.SetCollisionProfileName("trigger");
+ actor.StaticMeshComponent.bGenerateOverlapEvents = true;
+
+ var overlaps_disabled = false;
+ 
+ actor.OnActorBeginOverlap.Add((overlapped, other) => {
+  
+  if(overlaps_disabled){
+   return;
+  }
+  
+  overlaps_disabled = true;
+  
+  var spawned = @(unreal-js-fragment-content
+                  (if (procedure? exp)
+                      (exp)
+                      exp
+                      ));
+                        
+  overlaps_disabled = false;
+  })
+ })
+  )
 
 (define-classic-rune-lang my-mod-lang #:eval-from main.rkt
-  (hello))
+  (trigger-zone))
 
 (module+ main
   (codespells-workspace ;TODO: Change this to your local workspace if different
-   (build-path (current-directory) ".." "CodeSpellsWorkspace"))
+   (build-path (current-directory) ".." ".."))
   
   (once-upon-a-time
-   #:world (demo-world)
+   #:world (arena-world)
    #:aether (demo-aether
-             #:lang (my-mod-lang #:with-paren-runes? #t))))
+             #:lang (append-rune-langs #:name main.rkt
+                     (my-mod-lang #:with-paren-runes? #t)
+                     (rocks:my-mod-lang)
+                     (fire-particles:my-mod-lang)))))
 
 
